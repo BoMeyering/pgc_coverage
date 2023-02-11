0c04678968f943a021b5fe04027b75cd9dd8dbe0
bomeyering <bomeyering@mailbox.org>
2023-02-11 01:05:09 -0500

    added code to batch process images, and reproject masks onto original images

--- examples/main.py
+++ examples/main.py
@@ -3,13 +3,14 @@ import sys
 
 import cv2
 import numpy as np
+from glob import glob
 
 sys.path.insert(0,'../src')
 from detection import detectMarkers, momentCoord
 from utils import manualThreshold, showImage
 from linalg import closestCorner, imgTransform, imgCorners
 from processing import createMasks, maskROI, kmeans
-from pipeline import setParameters
+from pipeline import determineParams, imageProcess
 
 # img = cv2.imread('./images/blue_markers/IMG_1158.jpg')
 # params = setParameters('./images/blue_markers/IMG_1158.jpg')
@@ -38,5 +39,21 @@ from pipeline import setParameters
 # cv2.imwrite('kmeans.jpg', k_clust)
 # manualThreshold(filename='IMG_1163_transformed.jpg', invert=False)
 
-params = setParameters(filename='./images/blue_markers/IMG_1158.jpg', zones=5, aggregation=np.mean)
+params = determineParams(filename='./images/blue_markers/IMG_1156.jpg', zones=5, aggregation=np.mean)
 print(params)
+
+files = glob('./images/blue_markers/*')
+print(files)
+for idx, file in enumerate(files):
+    img_proc = imageProcess(filename=file)
+    img_proc.process(5, params)
+    img_proc._reprojectRaw()
+    # showImage(img_proc.reprojected)
+    # print(type(img_proc.reprojected))
+    cv2.imwrite(f'./images/output/{idx}_reprojected.jpg', img_proc.reprojected)
+    cv2.imwrite(f'./images/output/{idx}_maskedFull.jpg', img_proc.maskedZonesFull)
+# img_proc = imageProcess(filename='./images/blue_markers/IMG_1156.jpg')
+# img_proc.process(5, params)
+
+# print(img_proc.zoneProps)
+
--- src/pipeline.py
+++ src/pipeline.py
@@ -4,22 +4,125 @@
 import cv2
 import os
 from detection import *
-from processing import createMasks, maskROI
+from processing import createMasks, maskROI, recreateImg
 from linalg import closestCorner, imgTransform, imgCorners
 from utils import showImage
 
-def processImage(filename, **keywords):
+
+class imageProcess:
     """
 
-    :param filename:
-    :param keywords:
-    :return:
     """
-    pass
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.rawImg = None
+        self.threshold = None
+        self.markerContours = None
+        self.centers = None
+        self.srcCorners = None
+        self.transformedImg = None
+        self.zoneMasks = None
+        self.ROI = None
+        self.pgcMasks = None
+        self.maskedZones = None
+        self.zoneProps = None
+
+    def process(self, zones, threshold, output_size=(1200, 2400), preprocess=None):
+        """
+
+        :param zones:
+        :param threshold:
+        :param preprocess:
+        :return:
+        """
+
+        # Read in image
+        rawImg = cv2.imread(self.filename)
+        self.rawImg = rawImg
+
+        # Threshold Parameters
+        self.threshold = {'hsvMin': tuple([int(i) for i in threshold[0:3]]),
+                          'hsvMax': tuple([int(i) for i in threshold[3:]])}
+
+        # HSV parameters to threshold the blue markers
+        blueMin = (85, 120, 215)
+        blueMax = (179, 255, 255)
+
+        # Get contours of the corner markers
+        markerConts = detectMarkers(rawImg, blueMin, blueMax)
+        self.markerContours = markerConts
+
+        # Find marker moments and closest corners
+        centers = momentCoord(markerConts)
+
+        srcCorners = imgCorners(rawImg)
+        idx = closestCorner(srcCorners, centers)
+        centers = centers[idx, :]
+        self.srcCorners = srcCorners
+        self.centers = centers
+
+        # Transform the image to a rectangle
+        transformed = imgTransform(centers, rawImg, output_size)
+        # showImage(transformed)
+        masks = createMasks(transformed, zones=zones)
+        rois = maskROI(transformed, masks)
+        self.transformedImg = transformed
+        self.zoneMasks = masks
+        self.ROI = rois
+
+        pgcMasks = []
+        maskedZones = []
+        zoneProps = []
+        for i in range(zones):
+            ROI = rois[i]
+            zoneMask = self.zoneMasks[i]
+
+            # Threshold and mask the original ROI
+            hsvROI = cv2.cvtColor(ROI, cv2.COLOR_BGR2HSV)
+            # pgcMask = cv2.inRange(hsvROI, self.threshold['hsvMin'], self.threshold['hsvMax'])
+            pgcMask = cv2.inRange(hsvROI, self.threshold['hsvMin'], self.threshold['hsvMax'])
+
+            pgcMasks.append(pgcMask)
+            maskedZone = cv2.bitwise_and(ROI, ROI, mask=pgcMask)
+            maskedZones.append(maskedZone)
+
+            # Calculate proportion of cover
+            zoneProp = np.sum(pgcMask/255)/np.sum(zoneMask)
+            zoneProps.append(zoneProp)
+        self.pgcMasks = np.array(pgcMasks)
+        self.maskedZones = np.array(maskedZones)
+        self.zoneProps = np.array(zoneProps)
+        self.maskedFull = recreateImg(self.pgcMasks, self.transformedImg.shape[0:2])
+        self.maskedZonesFull = recreateImg(self.maskedZones, self.transformedImg.shape)
+
+    def _reprojectRaw(self):
+        """
+
+        :return:
+        """
+        roiCorners = imgCorners(self.maskedZonesFull)
+        # print(roiCorners)
+
+        M = cv2.getPerspectiveTransform(roiCorners, self.centers)
+        # print(M)
+        trImg = cv2.warpPerspective(self.maskedZonesFull, M, (self.rawImg.shape[1], self.rawImg.shape[0]))
+
+        rawImgBlack = self.rawImg
+        centers = self.centers.astype(np.int32)
+        centers = centers[[0, 1, 3, 2],:]
+        rawImgBlack = cv2.fillPoly(rawImgBlack, [centers], (0, 0, 0))
+
+        # combined = cv2.bitwise_or(rawImgBlack, trImg, mask=None)
+        combined = cv2.add(rawImgBlack, trImg)
+
+        self.reprojected = combined
 
 
 
-def setParameters(filename, zones=5, aggregation=np.mean):
+
+
+def determineParams(filename, zones=5, aggregation=np.mean):
     """
     Determine HSV image threshold parameters interactively
     :param filename: Absolute or relative filepath to image
@@ -103,7 +206,7 @@ def setParameters(filename, zones=5, aggregation=np.mean):
                     idx += 1
         cv2.destroyAllWindows()
         hsvArray = np.array(hsvArray)
-        hsvAgg = aggregation(hsvArray, axis=0)
+        hsvAgg = np.int32(aggregation(hsvArray, axis=0))
         return hsvAgg
 
 
@@ -128,76 +231,3 @@ def setParameters(filename, zones=5, aggregation=np.mean):
     return hsvParams
 
 
-
-
-
-
-
-
-
-
-
-def manualThreshold(filename, output='array', invert=True):
-    '''
-    Manual, interactive thresholding of images
-    Selects for pixels in chosen range
-    Returns a thresholded image Numpy Array
-    A dictionary of HSV threshold values chosen for the image
-    or both an array and dictionary
-    '''
-    assert output == 'array' or output == 'values' or output == 'both'
-    assert invert == True or invert == False
-    # rawImg = cv2.imread(filename)
-    hsvImg = cv2.cvtColor(filename, cv2.COLOR_BGR2HSV)
-
-    # hsvImg = cv2.cvtColor(rawImg, cv2.COLOR_BGR2HSV)
-
-    # empty function
-    def _doNothing(x):
-        pass
-
-    # create trackbar window and set starting values
-    cv2.namedWindow('Track Bars', cv2.WINDOW_NORMAL)
-    cv2.createTrackbar('Hmin', 'Track Bars', 0, 179, _doNothing)
-    cv2.createTrackbar('Smin', 'Track Bars', 0, 255, _doNothing)
-    cv2.createTrackbar('Vmin', 'Track Bars', 0, 255, _doNothing)
-    cv2.createTrackbar('Hmax', 'Track Bars', 0, 179, _doNothing)
-    cv2.createTrackbar('Smax', 'Track Bars', 0, 255, _doNothing)
-    cv2.createTrackbar('Vmax', 'Track Bars', 0, 255, _doNothing)
-    cv2.setTrackbarPos('Hmax', 'Track Bars', 179)
-    cv2.setTrackbarPos('Smax', 'Track Bars', 255)
-    cv2.setTrackbarPos('Vmax', 'Track Bars', 255)
-
-    cv2.namedWindow('Raw Image', cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow('HSV Image', cv2.WINDOW_KEEPRATIO)
-    cv2.namedWindow('Thresholding', cv2.WINDOW_KEEPRATIO)
-
-    cv2.imshow('Raw Image', rawImg)
-    cv2.imshow('HSV Image', hsvImg)
-
-    while True:
-        Hmin = cv2.getTrackbarPos('Hmin', 'Track Bars')
-        Smin = cv2.getTrackbarPos('Smin', 'Track Bars')
-        Vmin = cv2.getTrackbarPos('Vmin', 'Track Bars')
-        Hmax = cv2.getTrackbarPos('Hmax', 'Track Bars')
-        Smax = cv2.getTrackbarPos('Smax', 'Track Bars')
-        Vmax = cv2.getTrackbarPos('Vmax', 'Track Bars')
-
-        fImg = cv2.inRange(hsvImg, (Hmin, Smin, Vmin), (Hmax, Smax, Vmax))
-        cv2.imshow('Thresholding', fImg)
-        key = cv2.waitKey(25)
-        if key == ord('q'):
-            break
-
-    cv2.destroyAllWindows()
-
-    hsvValues = {'HSVmin': (Hmin, Smin, Vmin), 'HSVmax': (Hmax, Smax, Vmax)}
-    if invert == True:
-        fImg = cv2.bitwise_not(fImg)
-
-    if output == 'array':
-        return fImg
-    elif output == 'values':
-        return mask_values
-    elif output == 'both':
-        return fImg, hsvValues
